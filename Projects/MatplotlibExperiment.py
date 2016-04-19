bd219eb7bc1d918b50445570dd8c11c710fb2f06
DannyGotTea <danielgallagher500@gmail.com>
2016-04-18 22:45:43 -0700

    Basic Scatter Plot

--- Projects/MatplotlibExperiment.py
+++ Projects/MatplotlibExperiment.py
@@ -6,12 +6,10 @@ y = [6,7,8,2,4]
 x2 = [1,3,5,9,11]
 y2 = [7,8,2,4,2]
 
-fig1 = plt.bar(x, y, label='Bars1', color='r')
-fig1.plt.bar(x2, y2, label='Bars2', color='b')
+#plt.bar(x, y, label='Bars1', color='r')
+#plt.bar(x2, y2, label='Bars2', color='b')
 
 population_ages = [1,44,56,2,22,4,55,44,65,66,88,5,56,67,8,5,40,99,78,65,53]
-
-ids = [x for x in range(len(population_ages))]
 
 bins = [1,10,20,30,40,50,60,70,80,90,100,110,120,130]
 
@@ -24,4 +22,4 @@ plt.title('Title')
 
 plt.grid(True)
 
-fig1.show()
+plt.show()
